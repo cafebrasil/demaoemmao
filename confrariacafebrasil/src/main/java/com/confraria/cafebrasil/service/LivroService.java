2897d296c5d8e10bf2300713b32529ce1d5fa794
mederafael <mederafael@gmail.com>
2017-04-09 20:39:31 -0300

    Correção Named Querie Livros

--- confrariacafebrasil/src/main/java/com/confraria/cafebrasil/service/LivroService.java
+++ confrariacafebrasil/src/main/java/com/confraria/cafebrasil/service/LivroService.java
@@ -29,7 +29,7 @@ public class LivroService {
 	@TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
 	public List<Livro> listar() throws ConfrariaException {
 		try {
-			return service.findAll("Livro.listaTodos", Livro.class);
+			return service.findAll("Livro.listarTodos", Livro.class);
 		}catch (final Exception e) {
 			logger.info(String.format("Rest /livros - %s", e.getMessage()));
 			throw new ConfrariaException(e.getMessage());
