c71f6fcc21c21f2070846d5ed5997921d82946ff
mederafael <mederafael@gmail.com>
2017-04-09 20:03:33 -0300

    Correção do Charset resultado Rest

--- confrariacafebrasil/src/main/java/com/confraria/cafebrasil/rest/ConfrariaRest.java
+++ confrariacafebrasil/src/main/java/com/confraria/cafebrasil/rest/ConfrariaRest.java
@@ -4,23 +4,23 @@ import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.confraria.cafebrasil.logger.Log;
 import com.confraria.cafebrasil.service.LivroService;
+import com.confraria.cafebrasil.util.Resources;
 
 @Path("/v1/demaoemmao")
 public class ConfrariaRest {
-	
+
 	@Inject
 	Log logger;
-	
+
 	@Inject
 	private LivroService livroService;
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
+	@Produces(Resources.APPLICATION_JSON_UTF8)
 	@Path(value = "/livros")
 	public Response listarLivros() {
 		logger.info("Recuperar livros Café Brasil");
--- confrariacafebrasil/src/main/java/com/confraria/cafebrasil/util/Resources.java
+++ confrariacafebrasil/src/main/java/com/confraria/cafebrasil/util/Resources.java
@@ -1,8 +1,10 @@
 package com.confraria.cafebrasil.util;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.ws.rs.core.MediaType;
 
 public class Resources {
 
@@ -12,5 +14,8 @@ public class Resources {
 
     @Produces
 	public final static boolean debug = Boolean.TRUE;
+
+    @Produces @ApplicationScoped
+    public final static String APPLICATION_JSON_UTF8 = MediaType.APPLICATION_JSON +";"+ MediaType.CHARSET_PARAMETER + "=utf-8";
 
 }
